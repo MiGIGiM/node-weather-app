7bbd0447c9fc0b9d2e347cb82f8a5a2aaa94e51f
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-08-02 23:25:19 -0600

    add: Setup for heroku delpoyment

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,6 @@
-
 const weatherForm = document.querySelector('form')
-
 const search = document.querySelector('input')
-
 const msgOne = document.getElementById('msgOne');
-
 const msgTwo = document.getElementById('msgTwo');
 
 weatherForm.addEventListener('submit', (e) => {
@@ -14,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
   msgOne.textContent = 'loading...'
   msgTwo.textContent = ''
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+  fetch(`/weather?address=${location}`).then((res) => {
     res.json().then((data) => {
       if (data.error) { 
         msgOne.textContent = data.error;
